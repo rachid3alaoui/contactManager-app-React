aedd2d6bdbddfb40a22c59e55027d6f76b08b1e7
rachid3alaoui <rachid3alaoui@gmail.com>
2020-08-10 11:22:11 +0100

    Updates

---
+++ precache-manifest.087f7a51b120a4833de290e4273ed3ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cea5f0ec893c6d649e1859606da2fd",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "5802e6dd97f6dd48d247",
+    "url": "/contactmanager/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6316fb2ff6048663d5f7",
+    "url": "/contactmanager/static/css/main.a41b68d7.chunk.css"
+  },
+  {
+    "revision": "5802e6dd97f6dd48d247",
+    "url": "/contactmanager/static/js/2.036f41d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.036f41d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6316fb2ff6048663d5f7",
+    "url": "/contactmanager/static/js/main.3902b298.chunk.js"
+  },
+  {
+    "revision": "e155b645805d8202c894",
+    "url": "/contactmanager/static/js/runtime-main.8912a38d.js"
+  }
+]);
